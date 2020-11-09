4377897ece213d2a95afdb1b4dfd66f934dbfeec
99002683 <72071846+99002683@users.noreply.github.com>
2020-11-09 10:01:14 +0530

    Added Starters in Menu Items

--- Implementation/src/collections/restaurant/MenuItems.java
+++ Implementation/src/collections/restaurant/MenuItems.java
@@ -16,9 +16,9 @@ public class MenuItems {
     }
     
     public void dataBase() {
-        String [] MenuItemName = {"Lux Soap", "Fair n Lovely", "MTR"};
-        Double [] MenuItemPrice = {40.00d, 60.00d, 30.00d};
-        Integer [] MenuItemQuantity = {10, 6, 10};
+        String [] MenuItemName = {"Fries", "Salad", "Papad Dry", "Papad Fry", "Masala Papad Fry", "Dry Papad Fry"};
+        Double [] MenuItemPrice = {40.00d, 65.00d, 10.00d, 10.00d, 20.00d, 20.00d};
+        Integer [] MenuItemQuantity = {20, 10, 10, 15, 16, 20};
         
         for (int i=0; i < MenuItemName.length; i++) {
             this.MenuItems.add(new MenuItem(i+1, MenuItemName[i], MenuItemPrice[i], MenuItemQuantity[i]));
