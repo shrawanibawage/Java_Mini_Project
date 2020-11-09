390bda3f984bf2e9496515384cfb8c779b9186b7
99002683 <72071846+99002683@users.noreply.github.com>
2020-11-09 10:07:23 +0530

    Added Veg Rice Items in Menu Items

--- Implementation/src/collections/restaurant/MenuItems.java
+++ Implementation/src/collections/restaurant/MenuItems.java
@@ -16,9 +16,9 @@ public class MenuItems {
     }
     
     public void dataBase() {
-        String [] MenuItemName = {"Fries", "Salad", "Papad Dry", "Papad Fry", "Masala Papad Fry", "Dry Papad Fry"};
-        Double [] MenuItemPrice = {40.00d, 65.00d, 10.00d, 10.00d, 20.00d, 20.00d};
-        Integer [] MenuItemQuantity = {20, 10, 10, 15, 16, 20};
+        String [] MenuItemName = {"Fries", "Salad", "Papad Dry", "Papad Fry", "Masala Papad Fry", "Dry Papad Fry", "Veg Fried Rice", "Jeera Rice", "Ghee Rice", "Biryani Rice"};
+        Double [] MenuItemPrice = {40.00d, 65.00d, 10.00d, 10.00d, 20.00d, 20.00d, 60.00d, 60.00d, 70.00d, 50.00d};
+        Integer [] MenuItemQuantity = {20, 10, 10, 15, 16, 20, 15, 5, 5, 10};
         
         for (int i=0; i < MenuItemName.length; i++) {
             this.MenuItems.add(new MenuItem(i+1, MenuItemName[i], MenuItemPrice[i], MenuItemQuantity[i]));
