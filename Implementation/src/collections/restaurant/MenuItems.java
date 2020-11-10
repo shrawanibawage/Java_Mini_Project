a6bf0c858d5a2b421c754a53db7cfbcc270ba074
99002683 <72071846+99002683@users.noreply.github.com>
2020-11-10 16:57:29 +0530

    Updated MenuItems.java with extra database

--- Implementation/src/collections/restaurant/MenuItems.java
+++ Implementation/src/collections/restaurant/MenuItems.java
@@ -17,9 +17,10 @@ public class MenuItems {
                                                                 
     public void dataBase() {                                                //created database function to add item name, item price and item quantity into a new ArrayList of type MenuItem
         String [] MenuItemName = {"Fries", "Salad", "Papad Dry", "Papad Fry", "Masala Papad Fry", "Dry Papad Fry", "Veg Fried Rice", "Jeera Rice", "Ghee Rice", "Biryani Rice", 
-                                 "Chicken Fried Rice", "Chicken Biryani", "Egg Fried Rice"};
-        Double [] MenuItemPrice = {40.00d, 65.00d, 10.00d, 10.00d, 20.00d, 20.00d, 60.00d, 60.00d, 70.00d, 50.00d, 110.00d, 100.00d, 70.00d};
-        Integer [] MenuItemQuantity = {20, 10, 10, 15, 16, 20, 15, 5, 5, 10, 8, 9, 6};
+                                 "Chicken Fried Rice", "Chicken Biryani", "Egg Fried Rice", "Paneer Butter Masala", "Mutter Paneer", "Shahi Paneer", "Tawa Chapati", 
+                                  "Tandoori Butter Roti", "Plain Nan", "Onion Paratha", "Aloo Paratha"};
+        Double [] MenuItemPrice = {40.00d, 65.00d, 10.00d, 10.00d, 20.00d, 20.00d, 60.00d, 60.00d, 70.00d, 50.00d, 110.00d, 100.00d, 70.00d, 150.00d, 150.00d, 150.00d, 20.00d, 30.00d, 10.00d, 50.00d, 60.00d};
+        Integer [] MenuItemQuantity = {20, 10, 10, 15, 16, 20, 15, 5, 5, 10, 8, 9, 6, 10, 8, 9, 10, 11, 8, 4, 7};
         
         for (int i=0; i < MenuItemName.length; i++) {
             this.MenuItems.add(new MenuItem(i+1, MenuItemName[i], MenuItemPrice[i], MenuItemQuantity[i]));
