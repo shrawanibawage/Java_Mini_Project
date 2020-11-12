2702d987eeb5ec403e75fff79fec61d68de2a37c
99002684 <71598676+99002684@users.noreply.github.com>
2020-11-12 18:58:10 +0530

    Update Main.java

--- 3_Implementation/src/collections/restmanage/Main.java
+++ 3_Implementation/src/collections/restmanage/Main.java
@@ -1,7 +1,8 @@
 package collections.restmanage;
 
 public class Main {
-	 public static void main (String [] args) {
+	 public static void main (String [] args) 
+	 {
 	         //object creation
 		 new RestaurantScreen();                             
 	    }
